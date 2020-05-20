20108c8f7af98021875b8df6c8b012e8b7391443
RyomaY <docomoknps200uu@gmail.com>
2020-05-20 17:12:09 +0900

    Add: n番目とn+1番目の要素を比較してn+1番目が小さければn番目に代入する

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,7 +3,10 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+  if ($array[$i] > $array[$i + 1]) {
+    $replacement = $array[$i];
+    $array[$i] = $array[$i + 1];
+  }
 }
 // 修正はここまで
 
