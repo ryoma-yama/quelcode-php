2594d2bd29bd17bb4f59745faad4465db8768e1a
RyomaY <docomoknps200uu@gmail.com>
2020-05-21 00:48:36 +0900

    Fix: インデントの半角スペースを4つに修正 PSR-2に準拠するため

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,13 +3,13 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-  for ($j = 1; $j < count($array); $j++) {
-    if ($array[$i] > $array[$i + $j] && $array[$i + $j] !== null) {
-      $replacement = $array[$i];
-      $array[$i] = $array[$i + $j];
-      $array[$i + $j] = $replacement;
+    for ($j = 1; $j < count($array); $j++) {
+        if ($array[$i] > $array[$i + $j] && $array[$i + $j] !== null) {
+            $replacement = $array[$i];
+            $array[$i] = $array[$i + $j];
+            $array[$i + $j] = $replacement;
+        }
     }
-  }
 }
 // 修正はここまで
 
