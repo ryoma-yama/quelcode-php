52accd7530ce022ce0de0a6181e59f0050fde457
RyomaY <docomoknps200uu@gmail.com>
2020-05-21 15:23:07 +0900

    Fix: n+1番が要素数を超えないように修正

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,11 +4,11 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 $elements = count($array);
 for ($i = 0; $i < $elements; $i++) {
-    for ($j = 1; $j < $elements; $j++) {
-        if ($array[$i] > $array[$i + $j] && $array[$i + $j] !== null) {
-            $replacement = $array[$i];
-            $array[$i] = $array[$i + $j];
-            $array[$i + $j] = $replacement;
+    for ($j = 0; $j < $elements - 1; $j++) {
+        if ($array[$j] > $array[$j + 1] && $array[$j + 1] !== null) {
+            $replacement = $array[$j];
+            $array[$j] = $array[$j + 1];
+            $array[$j + 1] = $replacement;
         }
     }
 }
