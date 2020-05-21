06e65807a3c3ebf9d7643eddd7bbe9d86e043e78
RyomaY <docomoknps200uu@gmail.com>
2020-05-21 18:04:44 +0900

    Add: 最大値として決定した値の分は繰り返さない

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,13 +4,14 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 $elements = count($array);
 for ($i = 0; $i < $elements; $i++) {
-    for ($j = 0; $j < $elements - 1; $j++) {
+    for ($j = 0; $j < $elements - 1 - $determined; $j++) {
         if ($array[$j] > $array[$j + 1]) {
             $replacement = $array[$j];
             $array[$j] = $array[$j + 1];
             $array[$j + 1] = $replacement;
         }
     }
+    $determined++;
 }
 // 修正はここまで
 
