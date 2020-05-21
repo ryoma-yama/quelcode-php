92d9dc3c42a29fd6e835acfb381931706a88ca8b
RyomaY <docomoknps200uu@gmail.com>
2020-05-21 21:26:33 +0900

    Add: 要素の置き換えが行われなかった場合にはloop処理を終了する 並び替えが終わっていることを意味するため

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -5,14 +5,19 @@ $array = explode(',', $_GET['array']);
 $elements = count($array);
 $times = $elements - 1;
 for ($i = 0; $i < $elements; $i++) {
+    $replaced = false;
     for ($j = 0; $j < $times - $determined; $j++) {
         if ($array[$j] > $array[$j + 1]) {
             $replacement = $array[$j];
             $array[$j] = $array[$j + 1];
             $array[$j + 1] = $replacement;
+            $replaced = true;
         }
     }
     $determined++;
+    if (!$replaced) {
+        break;
+    }
 }
 // 修正はここまで
 
