79c6f2cf228f3086a48bb3e2a4086edba3eb052e
RyomaY <docomoknps200uu@gmail.com>
2020-05-22 17:21:39 +0900

    Add: getParameterが1以上の整数でないなら400:BadRequestを返す

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,6 +1,12 @@
 <?php
 $limit = $_GET['target'];
 
+// getParameterが1以上の整数でないなら400:BadRequestを返す
+if (!ctype_digit($limit) || $limit === '0') {
+    http_response_code(400);
+    exit();
+}
+
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
