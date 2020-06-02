5060ba78b9fcb93426f12f848b5b9f1494ed770a
RyomaY <docomoknps200uu@gmail.com>
2020-06-02 19:54:40 +0900

    Add: like.phpに直接飛んできてないかの判定を追加

--- html/php-challenge/like.php
+++ html/php-challenge/like.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
 require 'dbconnect.php';
-$id = $_REQUEST['id'];
 
-$like = $db->prepare('INSERT INTO likes SET member_id=?, post_id=?');
-$like->execute([$_SESSION['id'], $id]);
+// このページに直接飛んできてないかどうか
+if (isset($_SESSION['id'])) {
+    $id = $_REQUEST['id'];
+
+    $like = $db->prepare('INSERT INTO likes SET member_id=?, post_id=?');
+    $like->execute([$_SESSION['id'], $id]);
+}
 
 header('Location: index.php');
 exit();
