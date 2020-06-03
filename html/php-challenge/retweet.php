8fbaeb99e8db6874188e471e41dfbfbbc3bca128
RyomaY <docomoknps200uu@gmail.com>
2020-06-03 11:48:29 +0900

    Add: retweet.phpに直接飛んできてないかの判定を追加

--- html/php-challenge/retweet.php
+++ html/php-challenge/retweet.php
@@ -1,21 +1,25 @@
 <?php
 session_start();
 require 'dbconnect.php';
-$id = $_REQUEST['id'];
 
-// 複製する投稿のDataを取得する
-$getMessage = $db->prepare('SELECT message, member_id, reply_post_id, created FROM posts WHERE id=?');
-$getMessage->execute([$id]);
-$message = $getMessage->fetch();
+// このページに直接飛んできてないかどうか
+if (isset($_SESSION['id'])) {
+    $id = $_REQUEST['id'];
 
-// 投稿を複製する
-$retweet = $db->prepare('INSERT INTO posts SET message=?, member_id=?, reply_post_id=?, created=?');
-$retweet->execute([
-    $message['message'],
-    $message['member_id'],
-    $message['reply_post_id'],
-    $message['created']
-]);
+    // 複製する投稿のDataを取得する
+    $getMessage = $db->prepare('SELECT message, member_id, reply_post_id, created FROM posts WHERE id=?');
+    $getMessage->execute([$id]);
+    $message = $getMessage->fetch();
+
+    // 投稿を複製する
+    $retweet = $db->prepare('INSERT INTO posts SET message=?, member_id=?, reply_post_id=?, created=?');
+    $retweet->execute([
+        $message['message'],
+        $message['member_id'],
+        $message['reply_post_id'],
+        $message['created']
+    ]);
+}
 
 header('Location: index.php');
 exit();
