7a2c82552284332cc965818649ee2a157f2d2e93
RyomaY <docomoknps200uu@gmail.com>
2020-06-03 18:42:41 +0900

    Add: リツイートした先の投稿からリツイートを取り消す

--- html/php-challenge/retweet.php
+++ html/php-challenge/retweet.php
@@ -7,20 +7,30 @@ if (isset($_SESSION['id'])) {
     $id = $_REQUEST['id'];
 
     // 複製する投稿のDataを取得する
-    $getMessage = $db->prepare('SELECT message, member_id, reply_post_id, created FROM posts WHERE id=?');
+    $getMessage = $db->prepare('SELECT message, member_id, reply_post_id, retweet_member_id, created FROM posts WHERE id=?');
     $getMessage->execute([$id]);
     $message = $getMessage->fetch();
 
-    // 投稿を複製する
-    $retweet = $db->prepare('INSERT INTO posts SET message=?, member_id=?, reply_post_id=?, retweet_member_id=?, retweet_post_id=?, created=?');
-    $retweet->execute([
-        $message['message'],
-        $_SESSION['id'],
-        $message['reply_post_id'],
-        $message['member_id'],
-        $id,
-        $message['created']
-    ]);
+    // postsTableにあって
+    if ($message['member_id'] === $_SESSION['id'] && $message['retweet_member_id'] !== '0') {
+        // urlParameterでdisが渡されていれば
+        if ($_REQUEST['option'] === 'dis') {
+            // リツイートを取り消す
+            $disRetweet = $db->prepare('DELETE FROM posts WHERE id=?');
+            $disRetweet->execute([$id]);
+        }
+    } else {
+        // postsTableになければ, 投稿を複製する
+        $retweet = $db->prepare('INSERT INTO posts SET message=?, member_id=?, reply_post_id=?, retweet_member_id=?, retweet_post_id=?, created=?');
+        $retweet->execute([
+            $message['message'],
+            $_SESSION['id'],
+            $message['reply_post_id'],
+            $message['member_id'],
+            $id,
+            $message['created']
+        ]);
+    }
 }
 
 header('Location: index.php');
